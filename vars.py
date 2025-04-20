cbd2371032878d91553ab00b673d5b0c2f0262fd
Helladox <koreanserverff444@gmail.com>
2025-04-20 15:09:28 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,4 +4,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "22581733")) #Replace with your api id
 API_HASH = environ.get("API_HASH", "1db7bdcf908100cc641c6a5276765c3d") #Replace with your api hash
-BOT_TOKEN = environ.get("BOT_TOKEN", "7805023087:AAFJWj6pIoVaJscvtjrpGgD4QtoYYVh9aAc") #Replace with your bot token
+BOT_TOKEN = environ.get("BOT_TOKEN", "7651480119:AAHI9shRDU52ZL7sc4WWYpfHfNAR2pEzB9g") #Replace with your bot token
